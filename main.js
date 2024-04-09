dd73eaf22c0f03f908ce72fff99987b322dffb7b
proffcavalcanti <166457096+proffcavalcanti@users.noreply.github.com>
2024-04-09 14:13:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let nomeUsuário = "";
+let elemento = document.querySelector ("#nome-usuário");
+
+while(nomeUsuário ==""){
+    nomeUsuário =  prompt("Qual é o seu nome?");
+}
+if(nomeUsuário == null){
+    elemento.textContent = "Seja muito bem-vindo!";
+}else{elemento.textContent = nomeUsuário;
+
+}
